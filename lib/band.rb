10f93be5d0267c04eba7a644e255116d703cb45a
Jennifer Bolanos <jbolanos707@gmail.com>
2015-05-15 10:36:29 -0700

    validate the presence of band name and not save if no name has been entered

--- lib/band.rb
+++ lib/band.rb
@@ -1,6 +1,7 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
   before_save :titleize_name
+  validates(:name, presence: true)
 
   private
 
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -7,4 +7,9 @@ it('capitalizes the band name') do
     test_band = Band.create(name: "u2")
     expect(test_band.name).to(eq("U2"))
   end
+
+it('validates presence of name') do
+    test_band = Band.new(name: '')
+    expect(test_band.save()).to(eq(false))
+  end
 end
