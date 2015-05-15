ce5e6d7839d6a42ef7e06bb71f8b47f2691b4db7
Jennifer Bolanos <jbolanos707@gmail.com>
2015-05-15 10:31:57 -0700

    titeleize venue name

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,10 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
+  before_save :titleize_name
+
+  private
+
+  define_method(:titleize_name) do
+    self.name = (name().titleize())
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 describe(Venue) do
   it { should have_and_belong_to_many(:bands) }
+
+  it('capitalizes the venue name') do
+      test_venue = Venue.create(name: "the fillmore")
+      expect(test_venue.name).to(eq("The Fillmore"))
+  end
 end
