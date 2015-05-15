ffb948b501dcf800892ec31cf2493b8810d25e0e
Jennifer Bolanos <jbolanos707@gmail.com>
2015-05-15 11:19:05 -0700

    load index page, take user to bands page, and allow user to add a band

---
+++ app.rb
@@ -0,0 +1,24 @@
+require('bundler/setup')
+Bundler.require(:default)
+require('pry')
+also_reload('./lib/**/*.rb')
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/bands') do
+  @bands = Band.all
+  erb(:bands)
+end
+
+post('/bands') do
+  name = params.fetch('name')
+  new_band = Band.new(name: name)
+  if new_band.save()
+    redirect('/bands')
+  else
+    erb(:error)
+  end
+end
